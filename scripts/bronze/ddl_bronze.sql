da73836249a1afb41caa84571e44e77557ac21bd
Abhishek Tanwar <56914494+at9596@users.noreply.github.com>
2026-06-07 07:45:18 +0530

    Refactor DDL script for Bronze tables

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,17 +1,23 @@
-/*
-===============================================================================
-DDL Script: Create Bronze Tables
-===============================================================================
-Script Purpose:
-    This script creates tables in the 'bronze' schema, dropping existing tables 
-    if they already exist.
-	  Run this script to re-define the DDL structure of 'bronze' Tables
-===============================================================================
-*/
+USE master;
+
+IF DB_ID('DataWarehouse') IS NOT NULL
+BEGIN
+    ALTER DATABASE DataWarehouse
+    SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+
+    DROP DATABASE DataWarehouse;
+END;
+
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+CREATE SCHEMA bronze;
+CREATE SCHEMA silver;
+CREATE SCHEMA gold;
 
 IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
     DROP TABLE bronze.crm_cust_info;
-GO
 
 CREATE TABLE bronze.crm_cust_info (
     cst_id              INT,
@@ -22,11 +28,9 @@ CREATE TABLE bronze.crm_cust_info (
     cst_gndr            NVARCHAR(50),
     cst_create_date     DATE
 );
-GO
 
 IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
     DROP TABLE bronze.crm_prd_info;
-GO
 
 CREATE TABLE bronze.crm_prd_info (
     prd_id       INT,
@@ -37,11 +41,9 @@ CREATE TABLE bronze.crm_prd_info (
     prd_start_dt DATETIME,
     prd_end_dt   DATETIME
 );
-GO
 
 IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
     DROP TABLE bronze.crm_sales_details;
-GO
 
 CREATE TABLE bronze.crm_sales_details (
     sls_ord_num  NVARCHAR(50),
@@ -54,32 +56,26 @@ CREATE TABLE bronze.crm_sales_details (
     sls_quantity INT,
     sls_price    INT
 );
-GO
 
 IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
     DROP TABLE bronze.erp_loc_a101;
-GO
 
 CREATE TABLE bronze.erp_loc_a101 (
     cid    NVARCHAR(50),
     cntry  NVARCHAR(50)
 );
-GO
 
 IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
     DROP TABLE bronze.erp_cust_az12;
-GO
 
 CREATE TABLE bronze.erp_cust_az12 (
     cid    NVARCHAR(50),
     bdate  DATE,
     gen    NVARCHAR(50)
 );
-GO
 
 IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
     DROP TABLE bronze.erp_px_cat_g1v2;
-GO
 
 CREATE TABLE bronze.erp_px_cat_g1v2 (
     id           NVARCHAR(50),
@@ -87,4 +83,3 @@ CREATE TABLE bronze.erp_px_cat_g1v2 (
     subcat       NVARCHAR(50),
     maintenance  NVARCHAR(50)
 );
-GO
