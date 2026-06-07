8c416fe076a26853d16221e80dea8782e1de8504
Abhishek Tanwar <56914494+at9596@users.noreply.github.com>
2026-06-07 08:49:36 +0530

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -5,3 +5,44 @@ WITH (
     FIELDTERMINATOR = ',',
     TABLOCK
 );
+
+BULK INSERT bronze.crm_prd_info
+FROM '/var/opt/mssql/datasets/source_crm/prd_info.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+
+
+BULK INSERT bronze.crm_sales_details
+FROM '/var/opt/mssql/datasets/source_crm/sales_details.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+
+BULK INSERT bronze.erp_cust_az12
+FROM '/var/opt/mssql/datasets/source_crp/CUST_AZ12.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+
+BULK INSERT bronze.erp_loc_a101
+FROM '/var/opt/mssql/datasets/source_crp/LOC_A101.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+
+BULK INSERT bronze.erp_px_cat_g1v2
+FROM '/var/opt/mssql/datasets/source_crp/PX_CAT_G1V2.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
