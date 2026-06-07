62427760872e338242dbaca0d3780623240afcc6
Abhishek Tanwar <56914494+at9596@users.noreply.github.com>
2026-06-07 06:42:40 +0530

    Refactor database initialization script

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -12,31 +12,21 @@ WARNING:
     All data in the database will be permanently deleted. Proceed with caution 
     and ensure you have proper backups before running this script.
 */
-
 USE master;
-GO
 
--- Drop and recreate the 'DataWarehouse' database
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+IF DB_ID('DataWarehouse') IS NOT NULL
 BEGIN
-    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    ALTER DATABASE DataWarehouse
+    SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+
     DROP DATABASE DataWarehouse;
 END;
-GO
 
--- Create the 'DataWarehouse' database
 CREATE DATABASE DataWarehouse;
-GO
 
 USE DataWarehouse;
-GO
 
--- Create Schemas
 CREATE SCHEMA bronze;
-GO
-
 CREATE SCHEMA silver;
-GO
-
 CREATE SCHEMA gold;
-GO
+
